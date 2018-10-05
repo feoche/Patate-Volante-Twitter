61e2f0065df828a4256a92d1bffb5199cf10e9a6
feoche <francois.eoche@corp.ovh.com>
2018-10-05 15:16:28 +0200

    Add link

--- data.js
+++ data.js
@@ -103,7 +103,8 @@ const data = {
     `http://www.cnrtl.fr/definition/digital`,
     `http://toucher.rectal.digital`,
     `https://www.youtube.com/watch?v=2N7Qea39Ego`,
-    `Même au Québec : http://gdt.oqlf.gouv.qc.ca/ficheOqlf.aspx?Id_Fiche=2654099`
+    `Même au Québec : http://gdt.oqlf.gouv.qc.ca/ficheOqlf.aspx?Id_Fiche=2654099`,
+    `https://www.bbc.com/news/av/embed/p06n111v/45760646`
   ],
 
   MINFOLLOWERS : 100,
